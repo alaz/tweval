da6030bd3d8e229436525a80a1f3e276e597a72e
Alexander Azarov <azarov@osinka.ru>
2012-01-12 20:57:25 +0400

    more debugging info

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -3,8 +3,11 @@ package bootstrap.liftweb
 import net.liftweb.common.LazyLoggable
 
 class Boot extends LazyLoggable {
+  System.setProperty("eval.debug", "true")
+
   def boot {
     val eval = new com.twitter.util.Eval
+    logger info "impliedClasspath %s".format(eval.impliedClassPath)
     val config = eval.apply[Symbol]( classOf[Boot].getClassLoader getResourceAsStream "Config.scala")
     assert('Config == config)
     logger info "Started"
